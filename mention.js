eb22c18ec7ad7fde73ae999cf93aac4e230bbc58
Fabio Teixeira <fabio1079@gmail.com>
2016-08-02 14:14:59 -0300

    search and display user list
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- mention.js
+++ mention.js
@@ -1,18 +1,26 @@
 (function($, _, undefined) {
 
+  _.templateSettings = {
+    evaluate    : /<%([\s\S]+?)%>/g,
+    interpolate : /<%=([\s\S]+?)%>/g,
+    escape      : /<%-([\s\S]+?)%>/g,
+    variable    : "rc"
+  };
+
+
   function UserMentions(settup, $field) {
     this.settup = settup;
     this.$field = $field;
 
-    this.$field.on('keyup', this.keyupEvent.bind(this));
+    this.$field.on('input', this.userInputEvent.bind(this));
   }
 
 
-  UserMentions.prototype.keyupEvent = function keyupEvent(evt) {
+  UserMentions.prototype.userInputEvent = function userInputEvent(evt) {
     var hasMention = this.verifyIfHasMention();
 
     if (hasMention) {
-      console.log(this.getCurrentMention());
+      this.searchForMentions(this.getCurrentMention());
     }
   }
 
@@ -45,12 +53,57 @@
   }
 
 
+  UserMentions.prototype.searchForMentions = function searchForMentions(currentMention) {
+    if (_.isFunction(this.settup.getMentionsData)) {
+      this.settup.getMentionsData(currentMention, this.populateUserList.bind(this));
+    }
+  }
+
+
+  UserMentions.prototype.populateUserList = function populateUserList(userList) {
+    this.settup.userList = userList;
+
+    this.displayUserSelection();
+  }
+
+
+  UserMentions.prototype.displayUserSelection = function displayUserSelection() {
+    if ($(".user-mention-list").length === 1) {
+      $(".user-mention-list").remove();
+    }
+
+    var listTemplate = this.buildTemplates();
+
+    this.$field.after(listTemplate);
+  }
+
+
+  UserMentions.prototype.buildTemplates = function buildTemplates() {
+    var userListTemplate = this.settup.templates.userList({
+      userList: this.settup.userList
+    });
+
+    var containerTemplate = this.settup.templates.container({
+      userListTemplate: userListTemplate
+    });
+
+    return containerTemplate;
+  }
+
+
   $.fn.userMentions = function userMentions(settup) {
     _.defaults(settup, {
-      source: [],
+      userList: [],
+      getMentionsData: null,
       searchPattern: "^[a-z0-9]+(?:-[a-z0-9]+)*$", // by default it uses the slug pattern, EX: Hey @mary-jane look here !
       triggerChar: '@',
-      lengthStartSearch: 3
+      lengthStartSearch: 3,
+      templates: {
+        container: _.template("<div class='user-mention-list'><%= rc.userListTemplate %></div>"),
+        userList: _.template("<ul><% _.forEach(rc.userList, function(user) { %>" +
+                                "<li><%= user.slug %></li>" +
+                             "<% }); %></ul>")
+      }
     });
 
     var userMentions = new UserMentions(settup, this);
