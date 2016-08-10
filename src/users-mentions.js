a7bce213cba05ee5b8d4bb2ef06938963273e764
Fabio Teixeira <fabio1079@gmail.com>
2016-08-10 12:39:03 -0300

    Remove jQuery dependency
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- src/users-mentions.js
+++ src/users-mentions.js
@@ -1,4 +1,5 @@
-(function($, _, undefined) {
+(function(_, undefined) {
+  "use strict";
 
   _.templateSettings = {
     evaluate    : /<%([\s\S]+?)%>/g,
@@ -8,13 +9,51 @@
   };
 
 
-  function UserMentions(settup, $field) {
-    this.$wrapper = $("<div class='user-mention-wrapper'></div>");
-    this.settup = settup;
-    this.$field = $field;
+  function UserMentions(settup, field) {
+    var wrapper = document.createElement("div");
+    wrapper.classList.add("users-mention-wrapper");
 
-    this.$field.wrap(this.$wrapper);
-    this.$field.on('input', this.userInputEvent.bind(this));
+    field.parentNode.insertBefore(wrapper, field);
+    wrapper.appendChild(field);
+
+    this.settup = this.prepareSettup(settup);
+    this.field = field;
+    this.wrapper = wrapper;
+
+    this.field.addEventListener("input", this.userInputEvent.bind(this), false);
+  }
+
+
+  UserMentions.prototype.prepareSettup = function prepareSettup(settup) {
+    /*
+    if (typeof(settup) !== "object") {
+      settup = {};
+    }
+
+    settup.userList = settup.userList || [];
+    settup.getMentionsData = settup.getMentionsData || null;
+    // by default it uses the slug pattern, EX: Hey @mary-jane look here !
+    settup.searchPattern = settup.searchPattern || "^[a-z0-9]+(?:-[a-z0-9]+)*$";
+    settup.triggerChar = settup.triggerChar || '@';
+    settup.lengthStartSearch = settup.lengthStartSearch || 3;
+    settup.templates
+*/
+    _.defaults(settup, {
+      requestUrl: "",
+      userList: [],
+      getMentionsData: null,
+      searchPattern: "^[a-z0-9]+(?:-[a-z0-9]+)*$", // by default it uses the slug pattern, EX: Hey @mary-jane look here !
+      triggerChar: '@',
+      lengthStartSearch: 3,
+      templates: {
+        container: _.template("<div class='users-mention-list'><%= rc.userListTemplate %></div>"),
+        userList: _.template("<ul><% _.forEach(rc.userList, function(user) { %>" +
+                                "<li><%= user.slug %></li>" +
+                             "<% }); %></ul>")
+      }
+    });
+
+    return settup;
   }
 
 
@@ -41,8 +80,8 @@
 
 
   UserMentions.prototype.getCurrentMention = function getCurrentMention() {
-    var currentText = this.$field.val();
-    var cursorPosition = this.$field.prop("selectionStart");
+    var currentText = this.field.value;
+    var cursorPosition = this.field.selectionStart;
     var triggerPosition = currentText.lastIndexOf(this.settup.triggerChar, cursorPosition);
     var currentMention = "";
 
@@ -56,9 +95,29 @@
 
 
   UserMentions.prototype.searchForMentions = function searchForMentions(currentMention) {
-    if (_.isFunction(this.settup.getMentionsData)) {
-      this.settup.getMentionsData(currentMention, this.populateUserList.bind(this));
+    var xhr = new XMLHttpRequest();
+    var requestUrl = this.settup.requestUrl;
+    var params = "mention=" + encodeURIComponent(currentMention);
+    var currentInstance = this;
+
+    if (/^\/.*$/.test(requestUrl)) {
+      var currentLocation = window.location.href.replace(/\/$/, '');
+      requestUrl = currentLocation + requestUrl;
     }
+
+    xhr.open("GET", requestUrl);
+    xhr.onreadystatechange = function onreadystatechange() {
+      if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        currentInstance.populateUserList(response);
+      }
+    }
+
+
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Content-length', params.length);
+    xhr.send(params);
   }
 
 
@@ -70,15 +129,19 @@
 
 
   UserMentions.prototype.displayUserSelection = function displayUserSelection() {
-    if ($(".user-mention-list").length === 1) {
-      $(".user-mention-list").remove();
+    var userMentionList = this.wrapper.querySelector(".users-mention-list");
+
+    if (userMentionList) {
+      this.wrapper.removeChild(userMentionList);
     }
 
     var listTemplate = this.buildTemplates();
 
-    this.$field.after(listTemplate);
-    this.$field.off('keyup');
-    this.$field.on('keyup', this.fieldKeyUpEvent.bind(this));
+    //this.field.after(listTemplate);
+    this.field.insertAdjacentHTML("afterend", listTemplate);
+
+    this.field.removeEventListener("keyup", this.fieldKeyUpEvent.bind(this));
+    this.field.addEventListener("keyup", this.fieldKeyUpEvent.bind(this));
   }
 
 
@@ -106,27 +169,27 @@
 
 
   UserMentions.prototype.listKeyUpEvent = function listKeyUpEvent(evt) {
-    
+    console.log(evt);
   }
 
 
-  $.fn.userMentions = function userMentions(settup) {
-    _.defaults(settup, {
-      userList: [],
-      getMentionsData: null,
-      searchPattern: "^[a-z0-9]+(?:-[a-z0-9]+)*$", // by default it uses the slug pattern, EX: Hey @mary-jane look here !
-      triggerChar: '@',
-      lengthStartSearch: 3,
-      templates: {
-        container: _.template("<div class='user-mention-list'><%= rc.userListTemplate %></div>"),
-        userList: _.template("<ul><% _.forEach(rc.userList, function(user) { %>" +
-                                "<li><%= user.slug %></li>" +
-                             "<% }); %></ul>")
-      }
+  document.addEventListener("DOMContentLoaded", () => {
+    var elements = document.querySelectorAll("textarea[data-users-mention-field]");
+
+    [].forEach.call(elements, (el) => {
+      var dataAttr = [].filter.call(el.attributes, at => /^data-(?!users-mention)/.test(at.name));
+
+      var settup = {};
+      dataAttr.forEach((attr) => {
+        var name = attr.name.replace("data-", "");
+        name = name.replace(/-([a-z])/g, (_, firstLetter) => firstLetter.toUpperCase());
+        var value = attr.value;
+
+        settup[name] = value;
+      });
+
+      var userMentions = new UserMentions(settup, el);
     });
+  }, false);
 
-    var userMentions = new UserMentions(settup, this);
-
-    return this;
-  }
-}) (jQuery, _.runInContext());
+}) (_.runInContext());
